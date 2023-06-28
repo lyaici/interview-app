45d7bb8b76e3ea9bd4ce2c6ae4d7ed329606933e
Omar Mbengue <ombengue@sqli.com>
2023-06-28 22:34:54 +0000

    Feat :  implement rating ability

--- front/src/components/BeerItem/Rate/Rate.jsx
+++ front/src/components/BeerItem/Rate/Rate.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './Rate.module.scss'
 
-import styles from "./Rate.module.scss";
-export const Rate = ({ onRate }) => (
-  <div className={styles.rate}>
-    <div className={styles.inLove} />
-    <div className={styles.happy} />
-    <div className={styles.sad} />
-  </div>
-);
+// This is a list of different level objects wich contains a name property for the level's name and a value property wich represent the rate value
+const LEVELS = [
+    {
+        name: 'inLove',
+        value: 9,
+    },
+
+    {
+        name: 'happy',
+        value: 6,
+    },
+    {
+        name: 'sad',
+        value: 3,
+    },
+]
+export const Rate = ({ onRate }) => {
+    return (
+        <div className={styles.rate}>
+            {LEVELS.map(({ name, value }, index) => (
+                <div
+                    key={index}
+                    className={styles[name]}
+                    onClick={() => onRate(value)}
+                />
+            ))}
+        </div>
+    )
+}
+Rate.displayName = 'Rate'
+Rate.propTypes = {
+    onRate: PropTypes.func,
+}
