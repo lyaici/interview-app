e89d48bcf84a2f96ea4db38d5f379438bc38682a
NicolasDeGouveia <nicolas.degouveia@gmail.com>
2023-10-17 16:17:29 +0200

    solution in store.json

--- front/src/redux/store.js
+++ front/src/redux/store.js
@@ -2,6 +2,8 @@ import { createStore, applyMiddleware, compose } from "redux";
 import createSagaMiddleware from "redux-saga";
 import rootSaga, { rootReducer } from "./state";
 
+// Setup a verification : typeof window !== undefined && ...
+// In case of SSR
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const sagaMiddleware = createSagaMiddleware();
